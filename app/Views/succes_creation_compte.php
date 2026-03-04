561c745344467ff576eafd78fdc43c3ec7a2cbb2
micheldeagarta <michel@agarta-agency.fr>
2026-03-04 16:36:33 +0100

    final feature

--- app/Views/succes_creation_compte.php
+++ app/Views/succes_creation_compte.php
@@ -1,68 +1,68 @@
 <?php
-// CI4: Plus besoin de BASEPATH check
+$titre = $titre ?? 'REZO+ PC INLINE | Compte créé';
+$heading = $heading ?? 'Bienvenue dans REZO+ PC InLine';
+$footing = $footing ?? footer_html();
 ?><!DOCTYPE html>
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
-    <?php $assetVersion = urlencode(config('App')->appVersion); ?>
+    <link rel="icon" type="image/png" href="<?= base_url(); ?>images/icone_final_rezo_plus_PC_inline128.png">
+    <link rel="apple-touch-icon" href="<?= base_url(); ?>images/icone_final_rezo_plus_PC_inline128.png">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?= base_url('css/style.css?v='.$assetVersion) ?>"/>
-	<title><?php echo $titre;?></title>
+    <?php $assetVersion = urlencode(config('App')->appVersion); ?>
+    <link rel="stylesheet" href="<?= base_url('css/style.css?v=' . $assetVersion) ?>"/>
+	<title><?= esc($titre) ?></title>
     <script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-19724464-1']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		
-		</script>
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-19724464-1']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
 </head>
-<body>
+<body class="page-signup page-succes-creation">
 
-<div id="container">
-    <p style="text-align:center;">
-    <a href="<?php echo base_url();?>"><img border="0" alt="Rezo+ pc inline" src="<?php echo base_url();?>images/icone_final_rezo_plus_PC_inline128.png" width="100" height="100"></a>
-    </p>
-    
-	<h1 style="margin-bottom:0px;"><?php echo $heading;?></h1>
-<p style="font-size:10px;text-align:center;"><?= getenv('VERSION_DU_SOFT') ?? 'Version 5.1' ?></p>
-    
-        
-	<div id="container_formulaire">
-
-        <div id="formulaire">
-        
-            <p style="font-size:22px;text-align:center;";>Compte créé.</p>
-            
-            <hr/>
-            
-            <p style="font-size:18px;text-align:center;"><strong>Votre compte vient d'être créé !<br />Notez bien votre code d'identification REZO +</strong></p>
-            <p style="font-size:18px;text-align:center;border:1px solid;"><strong><?php echo $code_administrateur;?></strong></p>
-            <p style="font-size:14px;text-align:center;">Vous pouvez aussi utiliser ce code d'identification pour les applications REZO+ et REZO+ PC pour Iphone/Ipad et Android</p>
-            <p style="font-size:14px;text-align:center;">Votre clé de licence est valable jusqu'au :</p>
-            <p style="font-size:14px;text-align:center;"><?php echo $date_fin_validite_licence;?></p>
-            <p style="font-size:14px;text-align:center;">Un mail de confirmation vient de vous être envoyé...</p>
-            <p style="font-size:14px;text-align:center;">Vous pouvez maintenant vous connecter à REZO+ PC InLine</p>
-            <p style="text-align:center;";><a href="<?php echo base_url();?>signup/login" class="bouton_creer_un_compte">Me connecter.</a></p>
-
-            
+<div id="container" class="signup-container">
+    <div class="login-hero">
+        <div class="login-hero-content">
+            <h1 class="login-hero-title">REZO+ PC Inline</h1>
+            <hr class="login-separator">
+            <p class="login-hero-subtitle">Plateforme opérationnelle</p>
+            <hr class="login-separator">
+            <p class="login-hero-tagline">Coordination et information en temps réel</p>
+            <p class="login-hero-version"><?= getenv('VERSION_DU_SOFT') ?? 'Version 5.1' ?></p>
         </div>
-        <div style="height:30px;"></div>
-	</div>
-</div>
-    
-<div class="separator"></div>
+    </div>
 
-    <footer>
-    
-        <p class="footer"><?php if(isset($footing)) echo $footing;?></p>
-        
-    
-    </footer>
+    <div class="login-panel-wrapper signup-panel-wrapper">
+        <div class="login-panel signup-panel">
+            <div class="login-logo-top">
+                <a href="<?= base_url(); ?>" class="login-logo-link" title="Accueil"><img border="0" alt="Rezo+ pc inline" src="<?= base_url(); ?>images/icone_final_rezo_plus_PC_inline128.png" width="80" height="80"></a>
+            </div>
+
+            <h2 class="login-panel-title">Compte créé</h2>
+
+            <div class="login-form-box signup-form-box succes-creation-box">
+                <p class="login-helper-text succes-creation-intro"><strong>Votre compte vient d'être créé.</strong><br />Notez bien votre code d'identification REZO+ :</p>
+                <p class="succes-creation-code"><?= esc($code_administrateur ?? '') ?></p>
+                <p class="login-helper-text">Ce code sert aussi pour les applications REZO+ et REZO+ PC pour iPhone/iPad et Android.</p>
+                <hr class="login-separator">
+                <p class="login-helper-text">Votre clé de licence est valable jusqu'au : <strong><?= esc($date_fin_validite_licence ?? '') ?></strong></p>
+                <p class="login-helper-text">Un mail de confirmation vient de vous être envoyé.</p>
+                <p class="login-helper-text">Vous pouvez maintenant vous connecter à REZO+ PC InLine.</p>
+                <p class="login-submit">
+                    <a href="<?= base_url(); ?>signup/login" class="login-submit-link">Me connecter</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <p class="footer"><?= $footing ?></p>
+</footer>
 </body>
 </html>
